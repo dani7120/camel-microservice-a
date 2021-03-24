bffe295d63df0373c48ec86145b3e97e4ff9826b
Daniel Sanchez <sanchez7120@hotmail.com>
2021-03-23 22:02:27 -0600

    Added new choices

--- src/main/java/com/apachecamelcourse/microservices/camelmicroservicea/routes/MyFileRouter.java
+++ src/main/java/com/apachecamelcourse/microservices/camelmicroservicea/routes/MyFileRouter.java
@@ -3,11 +3,18 @@ package com.apachecamelcourse.microservices.camelmicroservicea.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
+                .routeId("My File Router ")
+                .choice()
+                    .when(simple("${file:ext} ends with 'xml'")) // if
+                        .log("XML FILE")
+                    .otherwise() // else
+                        .log("NOT an XML FILE")
+                .end()
                 .log("${body}")
                 .to("file:files/output");
     }
